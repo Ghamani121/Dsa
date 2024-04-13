a1869159e3a13614e7bd4549a77134a527746132
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-04-13 11:48:21 +0530

    update

---
+++ src/A2Functions/MaxnMin.java
@@ -0,0 +1,10 @@
+package A2Functions;
+
+import java.util.Scanner;
+
+public class MaxnMin {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        
+    }
+}
