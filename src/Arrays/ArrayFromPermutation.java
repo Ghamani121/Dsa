321d0d5b383ff68bf7bfbb6bc0407dceec8eac96
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-04-20 04:07:57 +0530

    day 12

--- src/Arrays/ArrayFromPermutation.java
+++ src/Arrays/ArrayFromPermutation.java
@@ -1,8 +1,33 @@
 package Arrays;
+import java.util.Arrays;
+import java.util.Scanner;
+//public class ArrayFromPermutation
+//{
+//    public static void main(String[] args) {
+//
+//    }
+//}
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int ans[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
 
-public class ArrayFromPermutation
-{
     public static void main(String[] args) {
-
+        Scanner in =new Scanner(System.in);
+        System.out.println("Enter array length");
+        int n=in.nextInt();
+        int nums[]=new int[n];
+        System.out.println("Enter array");
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=in.nextInt();
+        }
+        Solution s=new Solution();
+        System.out.println(Arrays.toString(s.buildArray(nums)));   //best way to print arrays
     }
 }
