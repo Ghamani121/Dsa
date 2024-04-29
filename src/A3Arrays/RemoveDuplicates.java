43f354d7aaef08b0555083b1f87277f3c98b6def
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-04-29 23:17:42 +0530

    21.3

---
+++ src/A3Arrays/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+package A3Arrays;
+
+
+class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int count=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
