9db10c86e9d0b462051599dcc2dc6094772abc22
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-05-12 01:48:17 +0530

    day 28

---
+++ src/Searching/Square.java
@@ -0,0 +1,16 @@
+package Searching;
+    class Square{
+        public int mySqrt(int x) {
+            int start=1,end=x,mid=0;
+            if(x==0 ||x==1) return x;
+            while(start<=end)
+            {
+                mid=start+(end-start)/2;
+                if((long)mid*mid>(long)x) end=mid-1;//adding long here prevents integer overflow
+                else if(mid*mid==0) return mid;
+                else start=mid+1;
+            }
+            return Math.round(end);
+        }
+    }
+
