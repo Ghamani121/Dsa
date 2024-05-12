e07c7e180ef7bb12c4e8df10042b9346bb6fecd7
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-05-12 23:37:18 +0530

    day 29

---
+++ src/Searching/guessGame.java
@@ -0,0 +1,25 @@
+package Searching;
+
+
+//  Forward declaration of guess API.
+//  @param  num   your guess
+//  @return 	     -1 if num is higher than the picked number
+// 			      1 if num is lower than the picked number
+//                otherwise return 0
+// int guess(int num);
+
+
+public class guessGame extends GuessGame {
+    public int guessNumber(int n) {
+        int first=1,last=n,mid=0,a;
+        while(first<=last)
+        {
+            mid=first+(last-first)/2;
+            a=guess(mid);
+            if(a==0) return mid;
+            else if(a==1) first=mid+1;
+            else last=mid-1;
+        }
+        return -1;
+    }
+}
