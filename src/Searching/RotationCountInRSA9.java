2d325f06551f00b01027de594674d98e3eef7607
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-05-20 23:20:30 +0530

    34

--- src/Searching/RotationCountInRSA9.java
+++ src/Searching/RotationCountInRSA9.java
@@ -1,6 +1,6 @@
 package Searching;
 
-public class RotationCountInRSA {
+public class RotationCountInRSA9 {
     public static void main(String[] args) {
         int[] a= {4,5,6,7,0,1,2};
         //rc=index of pivot +1
