7d8d6dd748ac4872b354ee9e5b27d0763ab4f8b0
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-05-24 09:46:32 +0530

    day 36.3

--- src/Searching/TwoSum14.java
+++ src/Searching/TwoSum14.java
@@ -1,22 +1,35 @@
 package Searching;
 
-public class TwoSum14 {
-        public int[] twoSum(int[] numbers, int target) {
-            for(int i=0;i<numbers.length-1;i++)
-            {
-                int start=i+1,end=numbers.length-1;
-                int temp=target-numbers[i];
-                while(start<=end)
-                {
-                    int mid=start+(end-start)/2;
-                    // if(i==mid) mid=i+1;   //use i+1 for initializing start rather than giving start as 0
-                    if(temp==numbers[mid]) return new int[]{i+1,mid+1};
-                    else if(temp>numbers[mid]) start=mid+1;
-                    else end=mid-1;
-                }
-            }
-            return new int[]{-1,-1};
+//public class TwoSum14 {
+//        public int[] twoSum(int[] numbers, int target) {
+//            for(int i=0;i<numbers.length-1;i++)
+//            {
+//                int start=i+1,end=numbers.length-1;
+//                int temp=target-numbers[i];
+//                while(start<=end)
+//                {
+//                    int mid=start+(end-start)/2;
+//                    // if(i==mid) mid=i+1;   //use i+1 for initializing start rather than giving start as 0
+//                    if(temp==numbers[mid]) return new int[]{i+1,mid+1};
+//                    else if(temp>numbers[mid]) start=mid+1;
+//                    else end=mid-1;
+//                }
+//            }
+//            return new int[]{-1,-1};
+//        }
+//    }
+//}
+//using binary approach here is redundant, use two pointers approach
+class TwoSum14
+{
+    public int[] twoSum(int[] nums, int target)
+    {
+        int start=0,end=nums.length-1;
+        while (nums[start]+nums[end]!=target)
+        {
+            if (nums[start]+nums[end]<target) start++;
+            else end--;
         }
+        return new int[]{start+1, end+1};
     }
 }
-//using binary approach here is redundant, use two pointers approach
