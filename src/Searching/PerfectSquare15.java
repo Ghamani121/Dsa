4442e8c0a48692de230a461e9a70b9da9cf0dfbe
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-05-26 00:54:15 +0530

    day 37

---
+++ src/Searching/PerfectSquare15.java
@@ -0,0 +1,16 @@
+package Searching;
+
+public class PerfectSquare15{
+        public boolean isPerfectSquare(int num) {
+            int start=0,end=num;
+            while(start<=end)
+            {
+                long mid=start+(end-start)/2;//used long to prevent overflow of mid variable
+                if(num==mid*mid) return true;
+                else if(num>mid*mid) start=(int)mid+1;//have to convert mid to int as it is the only way toadd it with ither int variables
+                else end=(int)mid-1;
+            }
+            return false;
+        }
+    }
+}
