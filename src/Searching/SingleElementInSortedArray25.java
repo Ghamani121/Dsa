6d2fb269b3302bf52fb4d828ef5de73ea1a999f3
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-06-09 23:36:59 +0530

    day 43

---
+++ src/Searching/SingleElementInSortedArray25.java
@@ -0,0 +1,21 @@
+package Searching;
+
+public class SingleElementInSortedArray25 {
+        public int singleNonDuplicate(int[] nums) {
+            int start=0,end=nums.length-1;
+            while(start<end)
+            {
+                int mid=start+(end-start)/2;
+                //if mid is even then mid+1 would contain duplicate of mid
+                //if mid is odd then mid-1 would contain duplicate of mid
+                //therefore,we move to right side of array and to check where the pattern is broken
+                //else if condition is not true, means pattern is broken and we need to move to left side of array to find where the singleelement exists
+                if(mid%2==0 && nums[mid+1]==nums[mid] || mid%2==1 && nums[mid-1]==nums[mid])
+                {
+                    start=mid+1;
+                }
+                else  end=mid;
+            }
+            return nums[end];
+        }
+    }
