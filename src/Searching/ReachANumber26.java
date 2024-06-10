985129a796e52a6cb2dd85e835954dfc4b428c46
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-06-11 01:33:13 +0530

    day 45

---
+++ src/Searching/ReachANumber26.java
@@ -0,0 +1,20 @@
+package Searching;
+
+public class ReachANumber26  {
+    public int reachNumber(int target) {
+        int sum=0,step=0;
+        target=Math.abs(target);//to convert target value to positive
+        while(sum<target)//get the steps equal or more than target
+        {
+            step++;
+            sum+=step;
+        }
+        while((sum-target)%2 != 0)//if value is odd, we cant move to negative side as it is 2*i
+        //aka we need difference to be even to move to left
+        {
+            step++;
+            sum+=step;
+        }
+        return step;
+    }
+}
