ebf529b4273a9391b188af0869da407d6c2356ce
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-06-11 23:49:25 +0530

    day 46

--- src/Searching/IntersectionOfElments19.java
+++ src/Searching/IntersectionOfElments19.java
@@ -2,23 +2,5 @@ package Searching;
 
 public class IntersectionOfElments19 {
         public int[] intersection(int[] nums1, int[] nums2) {
-            int n1=nums1.length,n2=nums2.length;
-            if(n1<n2)
-            {
-                for(int a:nums1)
-                {
-                    binarysearch(a,nums1);
-                }
-                binarysearch(int target,int[] nums1)
-                {
-                    int start=0,end=n1;
-                    while(start<end)
-                    {
-                        int mid=start+(end-start)/2;
-                        if(nums1[mid]==target) return nums1[mid];
-                    }
-                }
-            }
-
         }
     }
