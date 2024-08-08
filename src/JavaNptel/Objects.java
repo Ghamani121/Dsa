02fe010f1dc03c425bfe0c7d6ec314209a9e7661
ghamani <138302691+Ghamani121@users.noreply.github.com>
2024-08-08 15:12:53 +0530

    hmm

---
+++ src/JavaNptel/Objects.java
@@ -0,0 +1,15 @@
+package JavaNptel;
+
+public class Objects
+{
+    public name()
+    {
+
+    }
+    public static void main(String[] args) {
+        //create a new object
+        Object ob= new Object();
+        ob.name="ghamani";
+    }
+
+}
